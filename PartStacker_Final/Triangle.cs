38b1df1596919abee2c033beb185e2b64a7ffcd3
Braden Ganetsky <braden.ganetsky@gmail.com>
2025-01-16 22:24:20 -0600

    Remove unused Triangle constructor

--- PartStacker_Final/Triangle.cs
+++ PartStacker_Final/Triangle.cs
@@ -11,21 +11,13 @@ namespace PartStacker_Final
         public Point3 v1, v2, v3;
         public ushort Attribute;
 
-        public Triangle(Point3 normal, Point3 v1, Point3 v2, Point3 v3, ushort attribute)
+        public Triangle(Point3 normal, Point3 v1, Point3 v2, Point3 v3)
         {
             this.Normal = normal;
 
             this.v1 = v1;
             this.v2 = v2;
             this.v3 = v3;
-
-            this.Attribute = 0;
-        }
-
-        public Triangle(Point3 normal, Point3 v1, Point3 v2, Point3 v3)
-            : this(normal, v1, v2, v3, 0)
-        {
-
         }
 
         public Triangle Mirror()
@@ -35,17 +27,17 @@ namespace PartStacker_Final
 
         public Triangle Rotate(Point3 axis, float angle)
         {
-            return new Triangle(Normal.Rotate(axis, angle), v1.Rotate(axis, angle), v2.Rotate(axis, angle), v3.Rotate(axis, angle), Attribute);
+            return new Triangle(Normal.Rotate(axis, angle), v1.Rotate(axis, angle), v2.Rotate(axis, angle), v3.Rotate(axis, angle));
         }
 
         public Triangle Translate(Point3 offset)
         {
-            return new Triangle(Normal, v1 + offset, v2 + offset, v3 + offset, Attribute);
+            return new Triangle(Normal, v1 + offset, v2 + offset, v3 + offset);
         }
 
         public Triangle Scale(float factor)
         {
-            return new Triangle(Normal, v1 * factor, v2 * factor, v3 * factor, Attribute);
+            return new Triangle(Normal, v1 * factor, v2 * factor, v3 * factor);
         }
 
         public Point3[] Vertices
